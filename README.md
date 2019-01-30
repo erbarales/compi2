480d76d01980141a1bc817d07e2505a53b5e1602
Eddy Barales <eddy.barales@gmail.com>
2019-01-30 00:59:16 -0600

    MOD: Se cambian nombres de paquete.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # compi2
 Practicas de laboratorio
+
+Funcionando con 
+jdk 11.0.2
+maven 3.6.0
---
+++ practica/src/test/java/test/ParserUnitTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import ant.com.gt.Principal;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author eddyb
+ */
+public class ParserUnitTest {
+    
+    Principal p = new Principal();
+    
+    @Test
+    public void testParser() throws Exception {
+        String parsearArchivo = p.parsearArchivo("archivos_prueba/archivo_prueba.txt");
+        Assert.assertEquals("OK", parsearArchivo);
+    }        
+}
